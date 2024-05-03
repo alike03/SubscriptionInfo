eecbfea52c901afa781ea7962b29a7ef846cf1b0
ali.gueler <gueler@netengine.at>
2024-05-03 15:38:37 +0200

    added popup with current menu

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,65 +1,24 @@
-waitForElement('#store_controls #cart_status_data').then(function (element) {
-    let title = document.createElement('h1');
-    title.setAttribute('class', 'ag_changes_title');
-    title.innerText = "Loading...";
-
-    let xhr_data = document.createElement('div');
-    xhr_data.setAttribute('class', 'alike_xhr_data');
-
-    let container = document.createElement('div');
-    container.setAttribute('class', 'ag_changes');
-    container.appendChild(title);
-    container.appendChild(xhr_data);
-
-    let span = document.createElement('span');
-    span.setAttribute('class', 'pulldownButton');
-    span.innerText = "alike03's Subscription Info";
-    
-    let span2 = document.createElement('span');
-    span2.setAttribute('class', 'pulldownButton pulldown');
-    span.appendChild(span2);
-
-    let button = document.createElement('div');
-    button.setAttribute('id', 'ag_changes_button');
-    button.setAttribute('class', 'store_header_btn store_header_btn_gray');
-
-    button.appendChild(span);
-    button.appendChild(container);
-
-    if (save.options.menuToggle == 'mouseenter') {
-        button.onmouseenter = function () {
-            container.classList.remove('closed');
-            container.classList.add('open');
-        }
-        button.onmouseleave = function () {
-            container.classList.remove('open');
-            container.classList.add('closed');
-        }
-    } else {
-        button.onclick = function (e) {
-            // Was preventing menu clicks
-            if (!e.target.classList.contains('pulldownButton'))
-                return;
-            if (container.classList.contains('open')) {
-                container.classList.remove('open');
-                container.classList.add('closed');
-            } else {
-                container.classList.remove('closed');
-                container.classList.add('open');
-            }
-        };
-    }
-
-    element.querySelectorAll('#ag_changes_button').forEach((oldButton) => { 
-        oldButton.remove();
-    });
-    element.prepend(button);
-
-    loadChanges(save.options.timeFrame);
-});
-
 const allowedTags = ["h2","h3","h4","div","span","a","picture","source","img","ul","li","p","i","b","br","select","option","optgroup","input","label"];
 const allowedAttributes = ["id","class","style","href","srcset","loading","title","alt","target","value","type","name","for","placeholder","checked"];
+
+let title = document.createElement('h1');
+title.setAttribute('class', 'ag_changes_title');
+title.innerText = "Loading...";
+
+let xhr_data = document.createElement('div');
+xhr_data.setAttribute('class', 'alike_xhr_data');
+
+let container = document.createElement('div');
+container.setAttribute('class', 'ag_changes');
+container.appendChild(title);
+container.appendChild(xhr_data);
+
+// #replace child of querySelector content
+const options = document.querySelector('#options');
+options.removeChild(options.firstElementChild);
+options.appendChild(container);
+
+loadChanges(save.options.timeFrame);
 
 function createElementsFromJSON(content, parent) {
     if (content.hasOwnProperty('element')) {
